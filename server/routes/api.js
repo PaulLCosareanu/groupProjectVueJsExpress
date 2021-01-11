8238195b3af1e793b23ca69a3c17b92c3df6eaea
PaulLCosareanu <paulluciancosareanu@gmail.com>
2021-01-11 20:22:39 +0000

    client done

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,7 @@ const router=express.Router();
 
 //GETTERS**************************************************************************************************************************
 // Users***************************************************************************************************************************
-router.get('/users',(req,res)=>{
+router.get('/user',(req,res)=>{
     // res.send("hello")
     let user={
         "email":"user@email.com",
@@ -23,7 +23,15 @@ router.get('/lessons',(req,res)=>{
         {"topic":"Java","location":"London","price":900},
         {"topic":"Web App","location":"London","price":900},
         {"topic":"Web Dev","location":"London","price":1000},
-        {"topic":"IT","location":"London","price":1200}
+        {"topic":"IT","location":"London","price":1200},
+        {"topic":"math","location":"London","price":100},
+        {"topic":"math","location":"Liverpool","price":80},
+        {"topic":"math","location":"Oxford","price":90},
+        {"topic":"math","location":"Bristol","price":120},
+        {"topic":"Java","location":"London","price":900},
+        {"topic":"Web App","location":"London","price":900},
+        {"topic":"Web Dev","location":"London","price":1000},
+        
     ]
     res.send(lessons)
 })
